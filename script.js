619d30050c9e493039ecb216f99c3282528c70f0
Olivier NEUGNOT <oliviergenilac@gmail.com>
2025-03-17 14:01:22 +0100

    f

--- script.js
+++ script.js
@@ -2,29 +2,24 @@ const countText = document.getElementById("count");
 const btnClick = document.getElementById("btn-click");
 const newcount = document.querySelector(".new");
 const numberadd2 = document.querySelector(".numeradd");
-const btn = document.querySelector(".scnd-btn");
-
+const btntwo = document.querySelector(".scnd-btn");
+const compteur = document.getElementById("compteur");
+let res = 0;
+let res2 = 0;
 function clickone() {
-	let res = 0;
-	btnClick.addEventListener("click", () => {
-		res++;
-		countText.textContent = res;
-		if (res > 19) {
-			newcount.style.display = "flex";
-			btnClick.disabled = true;
-
-			btn.addEventListener("click", () => {
-				let res2 = 0;
-				res2++;
-				numberadd2.textContent = res2;
-				if (res2 > 100) {
-					anothorBtn.style.display = "flex";
-					scndBtn.disabled = true;
-				}
-			});
-		} else {
-			btnClick.disabled = false;
+	res++;
+	compteur.textContent = res + "20";
+	countText.textContent = res;
+	if (res > 19) {
+		res += 1;
+		compteur.textContent = res + "100";
+		if (res > 99) {
+			res += 3;
+			compteur.textContent = res + "500";
 		}
-	});
+	} else return;
 }
-clickone();
+
+btnClick.addEventListener("click", () => {
+	clickone();
+});
